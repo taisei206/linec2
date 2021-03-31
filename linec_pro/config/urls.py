115a5dc31bd1d75a05e24490022897922b7aae62
taisei <pneach@gamil.com>
2021-04-01 00:40:41 +0900

    '条件分岐試作品'

--- linec_pro/config/urls.py
+++ linec_pro/config/urls.py
@@ -4,4 +4,5 @@ from . import views
 app_name = 'config'
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
+    path('answer/', views.AnswerView, name="answer"),
 ]
--- linec_pro/config/views.py
+++ linec_pro/config/views.py
@@ -4,3 +4,12 @@ from django.views import generic
 
 class IndexView(generic.TemplateView):
     template_name = 'index.html'
+
+
+def AnswerView(request):
+    if request.GET.get('your_name')=="a":
+        return render(request, 'answer.html')
+    elif request.GET.get('your_name')=="b":
+        return render(request, 'answer1.html')
+    else:
+        return render(request, 'answer2.html')
